2915b233bebc55f9f82263509a51b57d35a017bb
Angela Ferrer <gelferrero01@gmail.com>
2024-01-24 01:32:05 +0800

    display message

---
+++ exercise_two.py
@@ -0,0 +1,5 @@
+# Print the sum of the current number and the previous number
+# Write a program to iterate the first 10 numbers, and in each iteration, print the sum of the current and previous number.
+
+# Display Message 
+print ('Printing sum of previous and current number in a range (10)...')
